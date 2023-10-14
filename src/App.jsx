e52b0bd1703b98ecf9dc53959992f92bec306bbb
partha2001ps <parthapn2017@gmail.com>
2023-10-14 11:52:03 +0530

    react complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-function App(){
-  return (<div>hello 
-    <p>partha</p>
+import './App.css'
+function App({ list1, list2, list3 }) {
+ 
+  return (<div>
+    <h1>React Price Card</h1>
+    <div className='full'>
+  <div className='card'>
+    <p className='plus' id='free'>FREE</p>
+    <h1 id='month'>$0/month</h1>
+    <ul id='unorder'>
+      {
+        list1.map(list =>
+          <li key={list.id}className='content'>
+          {list.important ? <p style={{color:'black'}}>✓ {list.content}</p> : <p style={{ color: 'lightgrey' }}>× {list.content}</p>}
+           </li>
+          )
+     }
+    </ul>
+    <button disabled>Button</button>
   </div>
+  <div className='card'>
+    <p className='plus'>PLUS</p>
+    <h1>$9/month</h1>
+    <ul id='unorder'>
+      {
+        list2.map(list =>
+          <li key={list.id}className='content'>
+          {list.important ? <p style={{color:'black'}}>✓ {list.content}</p> : <p style={{ color: 'lightgrey' }}>× {list.content}</p>}
+           </li>
+          )
+     }
+    </ul>
+    <button disabled>Button</button>
+  </div>
+  <div className='card'>
+    <p  className='plus'>PRO</p>
+    <h1>$49/month</h1>
+    <ul id='unorder'>
+      {
+        list3.map(list =>
+          <li key={list.id} className='content'>
+          {list.important ? <p style={{color:'black'}}>✓ {list.content}</p> : <p style={{ color: 'lightgrey' }}>× {list.content}</p>}
+           </li>
+          )
+     }
+    </ul>
+    <button>Button</button>
+  </div>
+</div></div>
   )
 }
 
